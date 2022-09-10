b4f12866aa6b6a6cf550fd08db3638501f1ad737
Tom Rowbotham <rowbothamt@cardiff.ac.uk>
2022-09-10 16:34:46 +0100

    Added baloo 2 font

--- personal-portfolio/pages/_app.tsx
+++ personal-portfolio/pages/_app.tsx
@@ -1,21 +1,32 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import CustomNavBar from "../components/navigation/CustomNavBar";
+import {createTheme, ThemeProvider} from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const { window } = pageProps;
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+function MyApp({Component, pageProps}: AppProps) {
+    const {window} = pageProps;
+    const container =
+        window !== undefined ? () => window().document.body : undefined;
 
-  return (
-    <div className="background-gradient">
-      <CustomNavBar container={container} />
+    const theme = createTheme({
+        typography: {
+            fontFamily: [
+                '"Baloo Bhaijaan 2"', "cursive"
+            ].join(','),
+        }
+    });
 
-      <Component {...pageProps} />
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="background-gradient">
+                <CustomNavBar container={container}/>
+
+                <Component {...pageProps} />
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
